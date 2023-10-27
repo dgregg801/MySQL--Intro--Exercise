f89f427cf4c4c3f0aaa2f1e7a40ec59d26e7f998
Darin Gregg <daringregg801@gmail.com>
2023-10-26 21:26:05 -0600

    SQL Joins Finished for real this time

--- SQL Joins.sql
+++ SQL Joins.sql
@@ -1,5 +1,4 @@
 /* joins: select all the computers from the products table:
-
 using the products table and the categories table, return the product name and the category name */
 SELECT  products.Name as 'Product Name',  categories.Name as 'Category Name'
 FROM products
@@ -28,27 +27,28 @@ ON categories.DepartmentID = departments.DepartmentID
 WHERE categories.Name = 'Appliances' or  categories.Name = 'Games';
 
 /* joins: find the product name, total # sold, and total price sold,
-
- for Eagles: Hotel California --You may need to use SUM() */
+for Eagles: Hotel California --You may need to use SUM() */
 SELECT products.ProductID, Name PricePerUnit From products
 INNER JOIN sales
 ON products.ProductID = sales.ProductID
 WHERE Name LIKE '%HOTEL%California%'
 GROUP BY sales.ProductID;
- 
  SELECT * FROM sales
  WHERE ProductID = 97;
+
 /* joins: find Product name, reviewer name, rating, and comment on the Visio TV. (only return for the lowest rating!) */
 SELECT Name AS 'Product Name', Reviewer, Rating, Comment FROM reviews
 INNER JOIN products
 ON reviews.ProductID = products.ProductID
 WHERE Name LIKE '%Visio%TV%' AND Rating = 1;
+
 -- ------------------------------------------ Extra - May be difficult
-
 /* Your goal is to write a query that serves as an employee sales report.
-
-
-
 This query should return the employeeID, the employee's first and last name, the name of each product, how many of that product they sold */
-SELECT EmployeeID, FirstName, LastName, Name, Quantity FROM employees
+SELECT sales.EmployeeID, FirstName, LastName, Name, SUM(Quantity) FROM employees
 INNER JOIN sales
+ON employees.EmployeeID = sales.EmployeeID
+INNER JOIN products
+ON sales.ProductID = products.ProductID
+GROUP BY sales.EmployeeID, sales.ProductID
+ORDER BY sales.EmployeeID, sales.ProductID;
